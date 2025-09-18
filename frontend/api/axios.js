35f7e3b84525652e8cdf07b70a6effd975c8dda2
wayuumunay <titooc@gmail.com>
2025-09-18 09:55:59 -0300

    Debug Vercel environment variable

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 // --- LÍNEA DE PRUEBA PARA VERCEL ---
-console.log("VITE_API_URL leída durante la construcción:", import.meta.env.nexocargo-production.up.railway.app);
+console.log("VITE_API_URL leída durante la construcción:", import.meta.env.VITE_API_URL);
 
 
 // Creamos una instancia de axios con la URL base de nuestro backend
