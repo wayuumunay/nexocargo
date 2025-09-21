8c69950dda6db56044f1833e5e6c4fd1c7dc19ec
wayuumunay <titooc@gmail.com>
2025-09-21 08:17:25 -0300

    Fix: agregar configuración de CORS en index.js

--- backend/index.js
+++ backend/index.js
@@ -8,18 +8,25 @@ const tripRoutes = require('./routes/trips');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
-const PORT = 3001;
+// Usamos el puerto que nos asigne Render/Railway en producción, o el 3001 en tu computadora
+const PORT = process.env.PORT || 3001;
+
+// --- CONFIGURACIÓN DE CORS PARA PRODUCCIÓN ---
+// Le decimos al backend que solo acepte peticiones desde la URL de tu frontend
+const corsOptions = {
+  origin: 'https://nexocargo.vercel.app' 
+};
 
 // Middlewares
-app.use(cors());
+app.use(cors(corsOptions)); // Usamos la nueva configuración
 app.use(express.json());
 
 // Rutas
 app.use('/api/users', userRoutes); 
 app.use('/api/trips', tripRoutes);
-app.use('/api/admin', adminRoutes); // Esta línea faltaba
+app.use('/api/admin', adminRoutes);
 
 // Arranque del Servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
